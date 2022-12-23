6d7519f3384bbab103144956ca712db53db66a3b
Sergey <92057970+sergstorozhev1984@users.noreply.github.com>
2022-12-23 13:53:06 +0200

    Profile html

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import { Profile } from "./Profile/Profile";
+import user from "assets/user.json";
+
 export const App = () => {
   return (
     <div
@@ -10,7 +13,11 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}/>
     </div>
   );
 };
